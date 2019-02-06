e1f74dc1a0d5920789b87c77b79f9f8ad43d583d
il <ilmak@o2.pl>
2019-02-06 17:12:19 +0100

    dziala

--- src/main/java/Cwiczenie5/Program.java
+++ src/main/java/Cwiczenie5/Program.java
@@ -1,9 +1,34 @@
 package Cwiczenie5;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Program {
     public static void main (String [] args){
 
-
         System.out.println("A kuku");
+
+        List<Osoba> listaOsob = new ArrayList<Osoba>();
+
+        Scanner odczyt = new Scanner (System.in);
+
+        for (int i =1; i<11;i++){
+            String imie;
+            String nazwisko;
+            System.out.println("Podaj imie dla osoby "+ i);
+            imie = odczyt.nextLine();
+            System.out.println("Podaj nazwisko dla osoby "+ i);
+            nazwisko = odczyt.nextLine();
+            listaOsob.add(new Osoba(imie,nazwisko));
+
+        }
+        System.out.println("Rozmiar listy to: "+listaOsob.size());
+        System.out.println("juz mam dane");
+
+        for (int i = listaOsob.size(); i>0; i--) {
+            System.out.println("imie " + listaOsob.get(i-1).getImie() + " nazwisko " + listaOsob.get(i-1).getNazwisko());
+        }
     }
 }
